d2eb5248e4db15798414b64491117c2599b472ab
gaddganepola <ganeypola@gmail.com>
2025-11-26 08:56:54 +0530

    add data annotation

--- src/main/java/com/ddg/SpringSecSection04/model/Customer.java
+++ src/main/java/com/ddg/SpringSecSection04/model/Customer.java
@@ -1,9 +1,15 @@
 package com.ddg.SpringSecSection04.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     @Id
