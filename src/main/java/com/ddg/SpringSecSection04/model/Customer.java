af17aba5e9447aa659ea3aefb338e1df3d8997ed
gaddganepola <ganeypola@gmail.com>
2025-11-26 08:54:14 +0530

    develop customer model

---
+++ src/main/java/com/ddg/SpringSecSection04/model/Customer.java
@@ -0,0 +1,17 @@
+package com.ddg.SpringSecSection04.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String pwd;
+    @Column(name = "role")
+    private String role;
+
+}
